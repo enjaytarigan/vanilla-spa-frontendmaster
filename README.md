5bfd7920df50400ba169d9714f812fcadd97c2cd
Enjay Tarigan <tariganagc@gmail.com>
2024-07-22 18:30:48 +0700

    create server using express

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
+# You Might Not Need a Framework
+
 This repo is a companion to the [You Don't Need That Library][course] course on Frontend Masters.
 
-[![Frontend Masters](images/FrontendMastersLogo.png)][fem]
+[![Frontend Masters](public/images/FrontendMastersLogo.png)][fem]
 
 [Please click here][website] to head to the course website.
 
 [fem]: https://www.frontendmasters.com
 [website]: https://firtman.github.io/vanilla/
 [course]: https://frontendmasters.com/courses/vanilla-js-apps/
+
+## Running the project
+
+-   Prerequisites
+
+    -   Node.js
+    -   NPM
+
+-   Steps to run the project
+    -   npm install
+    -   npm run start
+-   Access the application
+
+    Open web browser and navigate to http://localhost:8080
--- index.js
+++ index.js
@@ -1,70 +1,14 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
 
-const port = 8080;
-const publicDir = path.join(__dirname, "public");
+const server = express();
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    // Extract the requested URL path
-    const urlPath = req.url === "/" ? "/index.html" : req.url;
+server.use(express.static("public"));
 
-    // Build the file path
-    const filePath = path.join(publicDir, urlPath);
-
-    // Check if the file exists
-    fs.exists(filePath, (exists) => {
-        if (exists) {
-            // Serve the file if it exists
-            fs.readFile(filePath, (err, data) => {
-                if (err) {
-                    res.writeHead(500, { "Content-Type": "text/plain" });
-                    res.end("Internal Serve r Error");
-                } else {
-                    res.writeHead(200, {
-                        "Content-Type": getContentType(filePath),
-                    });
-                    res.end(data);
-                }
-            });
-        } else {
-            // Serve index.html for SPA routing
-            fs.readFile(path.join(publicDir, "index.html"), (err, data) => {
-                if (err) {
-                    res.writeHead(500, { "Content-Type": "text/plain" });
-                    res.end("Internal Server Error");
-                } else {
-                    res.writeHead(200, { "Content-Type": "text/html" });
-                    res.end(data);
-                }
-            });
-        }
-    });
+server.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+server.listen(8080, () => {
+    console.log(`server running on port ${8080}`);
 });
-
-// Function to get the content type based on file extension
-function getContentType(filePath) {
-    const ext = path.extname(filePath).toLowerCase();
-    switch (ext) {
-        case ".html":
-            return "text/html";
-        case ".js":
-            return "application/javascript";
-        case ".css":
-            return "text/css";
-        case ".json":
-            return "application/json";
-        case ".png":
-            return "image/png";
-        case ".jpg":
-        case ".jpeg":
-            return "image/jpeg";
-        default:
-            return "application/octet-stream";
-    }
-}
